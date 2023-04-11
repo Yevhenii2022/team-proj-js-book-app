16110ab9afc30ca877604c64006810973e6ad3f4
Yevhenii2022 <3starosta3@gmail.com>
2023-04-11 16:09:16 +0300

    code builder setup

---
+++ src/index.js
@@ -0,0 +1,15 @@
+//IMPORTS
+// import './js/pagination';
+console.log(555);
+
+// приклади імпортів NPM пакетів
+
+// import axios from 'axios';
+
+// import Notiflix from 'notiflix';
+
+// https://www.npmjs.com/package/spin.js?ref=vanillalist - посилання на спінер
+// import { Spinner } from 'spin.js';
+
+// https://www.npmjs.com/package/parcel-transformer-hbs - посилання на шаблонізатор
+// import CardTemplate from '../templates/gallery-card.hbs';
