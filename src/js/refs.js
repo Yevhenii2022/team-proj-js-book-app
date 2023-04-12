50c65ab0c45ff5a7680e8c9e19236aa737e4c9e7
Yevhenii2022 <3starosta3@gmail.com>
2023-04-12 21:13:43 +0300

    add file refs

---
+++ src/js/refs.js
@@ -0,0 +1,14 @@
+export default refs;
+
+const refs = {
+  //Header
+  // headerEl: document.querySelector('.header'),
+  // Home
+  //Shopping-list
+  //Pop-up-modal
+  // Mobile - menu - modal
+  // Authorization - modal
+  //Pagination
+  // Spinner/loader
+  // Scroll up
+};
