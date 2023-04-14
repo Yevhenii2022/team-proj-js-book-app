54a78dec0833cec0ad71dbdd102f239828925d12
Yevhenii2022 <3starosta3@gmail.com>
2023-04-14 15:52:10 +0300

    render book-card

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,20 +1,20 @@
 const refs = {
   //Header
-  // headerEl: document.querySelector('.header'),
+  headerEl: document.querySelector('.header'),
 
   // Home
+  categoriesSidebar: document.querySelector('.sidebar__categories'),
+  homeBooksByType: document.querySelector('.home-typeBook'),
+  homeBook: document.querySelector('.home-book'),
+  homeList: document.querySelector('.home-list'),
 
   //Shopping-list
 
   //Pop-up-modal
   openModalPopBtn: document.querySelector('[data-modal-open]'),
   closeModalPopBtn: document.querySelector('[data-modal-close]'),
-  modalPop: document.querySelector('[data-modal]'),
+  modalPopEl: document.querySelector('[data-modal]'),
   backdrop: document.querySelector('.backdrop'),
-  categoriesSidebar: document.querySelector('.sidebar__categories'),
-  homeBooksByType: document.querySelector('.home-typeBook'),
-  homeBook:document.querySelector('.home-book'),
-  homeList: document.querySelector('.home-list'),
 
   // Mobile - menu - modal
 
@@ -28,4 +28,3 @@ const refs = {
 };
 
 export default refs;
-
