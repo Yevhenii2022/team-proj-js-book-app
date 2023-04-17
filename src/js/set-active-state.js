6e645ff79c0db95d374d998686c7f724d653d33e
Tetiana Nivchyk <tata.02od@gmail.com>
2023-04-17 23:53:06 +0300

    fixed

--- src/js/set-active-state.js
+++ src/js/set-active-state.js
@@ -1,10 +1,10 @@
 export function setActiveState(elements) {
-  elements.forEach(item => {
+  elements.forEach((item, index) => {
     const href = item.getAttribute('href').match(/\/[a-z-]*.html/g);
     const windowHref = window.location.href.match(/team-proj-js-book-app\/$/g);
     if (
       window.location.href.includes(href) ||
-      window.location.href.includes(windowHref)
+      (window.location.href.includes(windowHref) && index === 0)
     ) {
       item.classList.add('is-activ');
     }
