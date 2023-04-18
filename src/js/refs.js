d5e90958f515090c01ebdff2cc970f1a1728757a
Yevhenii2022 <3starosta3@gmail.com>
2023-04-18 12:07:32 +0300

    modal

--- src/js/refs.js
+++ src/js/refs.js
@@ -20,7 +20,7 @@ const refs = {
   SHOP_LIST_KEY: 'selected-books',
 
   //Pop-up-modal
-  listBookEl: document.querySelector('.books__container'),
+  listBookEl: document.querySelector('.main'),
   closeModalPopBtn: document.querySelector('[data-modal-close]'),
   modalPopEl: document.querySelector('[data-modal]'),
   backdrop: document.querySelector('.backdrop'),
